388a5f1670f607c6a85b6703c6228863ff67c98a
E_Bwill <cy_sniper@yeah.net>
2019-06-14 17:33:12 +0800

    fix

--- script/install.sh
+++ script/install.sh
@@ -7,4 +7,4 @@ curl -o /etc/systemd/system/smith.service http://10.18.19.41/smith.service
 chmod 755 /opt/smith/smithworker1
 systemctl daemon-reload
 systemctl restart smith.service
-systemctl enable smith.service⏎
+systemctl enable smith.service
