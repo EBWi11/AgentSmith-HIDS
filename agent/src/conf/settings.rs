5c0d55f1e2daeedc0992ffe8f4cffe9b859799f3
E_Bwill <cy_sniper@yeah.net>
2019-09-03 10:21:42 +0800

    fix default conf

--- agent/src/conf/settings.rs
+++ agent/src/conf/settings.rs
@@ -5,8 +5,8 @@ pub const AUTO_INSTALL_LKM: bool = false;
 pub const SEND_KAFKA_FAST_TYPE : bool = false;
 
 pub const DAEMON: bool = true;
-pub const PID_FILE_PATH: &str = "/var/run/smith.pid";
-pub const SMITH_LOG_FILE: &str = "/var/log/smith.log";
+pub const PID_FILE_PATH: &str = "/var/run/smith_hids.pid";
+pub const SMITH_LOG_FILE: &str = "/var/log/smith_hids.log";
 pub const LKM_SERVER: &str = "http://10.18.18.18/";
 pub const LKM_TMP_PATH: &str = "/tmp/YWdlbnRzbWl0aGJ5ZWJ3aWxs";
 
