fb608b814726e595a1b740e66ba4485191a4f61a
chenyue@julanling.com <chenyue@julanling.com>
2019-11-03 01:03:38 +0800

    fix kretprobe

---
+++ syshook/kretprobe/share_mem.h
@@ -0,0 +1,37 @@
+/*******************************************************************
+* Project:	AgentSmith-HIDS
+* Author:	E_BWill
+* Year:		2018
+* File:		smith_hook.h
+* Description:	share memory
+*******************************************************************/
+#include <linux/mm.h>
+#include <linux/device.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+
+#define DEVICE_NAME "smith"
+#define CLASS_NAME "smith"
+
+#define MAX_SIZE 2097152
+#define CHECK_READ_INDEX_THRESHOLD 524288
+#define CHECK_WRITE_INDEX_THRESHOLD 32768
+
+#define DELAY_TEST 0
+#define KERNEL_PRINT 0
+
+static int share_mem_flag;
+
+struct msg_slot {
+    int len;
+    int next;
+};
+
+struct sh_mem_list_head {
+    int read_index;
+    int next;
+};
+
+int init_share_mem(void);
+int send_msg_to_user(char *msg, int kfree_flag);
+void uninstall_share_mem(void);
--- syshook/kretprobe/smith_hook.c
+++ syshook/kretprobe/smith_hook.c
@@ -17,12 +17,10 @@
 *
 * see <https://www.gnu.org/licenses/>.
 *******************************************************************/
-
-#include "smith_hook.h"
+#include "share_mem.h"
 #include <asm/syscall.h>
 #include <linux/kprobes.h>
 #include <linux/binfmts.h>
-#include <linux/device.h>
 #include <linux/fdtable.h>
 #include <linux/file.h>
 #include <linux/fs.h>
@@ -30,25 +28,18 @@
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
-#include <linux/mm.h>
 #include <linux/module.h>
 #include <linux/net.h>
-#include <linux/netlink.h>
 #include <linux/sched.h>
 #include <linux/skbuff.h>
 #include <linux/syscalls.h>
 #include <linux/utsname.h>
 #include <linux/version.h>
-#include <linux/spinlock.h>
 #include <linux/types.h>
 #include <linux/ptrace.h>
 #include <linux/namei.h>
 #include <net/inet_sock.h>
 #include <net/tcp.h>
-#include <net/inet_connection_sock.h>
-
-#define DEVICE_NAME "smith"
-#define CLASS_NAME "smith"
 
 #define EXECVE_TYPE "59"
 #define CONNECT_TYPE "42"
@@ -59,15 +50,7 @@
 #define DNS_TYPE "601"
 #define CREATE_FILE "602"
 
-#define MAX_SIZE 2097152
-#define CHECK_READ_INDEX_THRESHOLD 524288
-#define CHECK_WRITE_INDEX_THRESHOLD 32768
-
-#define KERNEL_PRINT 0
 #define EXIT_PROTECT 0
-#define DELAY_TEST 0
-#define WRITE_INDEX_TRY_LOCK 0
-#define WRITE_INDEX_TRY_LOCK_NUM 3
 
 #define CONNECT_HOOK 1
 #define ACCEPT_HOOK 1
@@ -107,19 +90,6 @@ char fsnotify_kretprobe_state = 0x0;
 char ptrace_kretprobe_state = 0x0;
 char recvfrom_kretprobe_state = 0x0;
 char load_module_kretprobe_state = 0x0;
-
-static char *list_head_char;
-static int check_read_index_flag = -1;
-static int share_mem_flag = -1;
-static int pre_slot_len = 0;
-static int write_index = 8;
-static struct class *class;
-static struct device *device;
-static int major;
-static char *sh_mem = NULL;
-static DEFINE_MUTEX(mchar_mutex);
-
-static rwlock_t _write_index_lock;
 
 struct connect_data {
     int fd;
@@ -169,243 +139,6 @@ static int checkCPUendian(void)
 unsigned short int Ntohs(unsigned short int n)
 {
     return checkCPUendianRes ? n : BigLittleSwap16(n);
-}
-
-#if DELAY_TEST == 1
-static char *get_timespec(void)
-{
-    char *res = NULL;
-    struct timespec tmp_time;
-    res = kzalloc(64, GFP_ATOMIC);
-    tmp_time = current_kernel_time();
-    snprintf(res, 64, "%lu.%lu", tmp_time.tv_sec, tmp_time.tv_nsec);
-    return res;
-}
-#endif
-
-static void lock_init(void)
-{
-    rwlock_init(&_write_index_lock);
-}
-
-#if WRITE_INDEX_TRY_LOCK == 1
-static int write_index_trylock(void)
-{
-    return write_trylock(&_write_index_lock);
-}
-#endif
-
-static inline void write_index_lock(void)
-{
-    write_lock(&_write_index_lock);
-}
-
-static inline void write_index_unlock(void)
-{
-    write_unlock(&_write_index_lock);
-}
-
-static void do_init_share_mem(int type)
-{
-    struct sh_mem_list_head _init_list_head = {0, -1};
-    if (type == 1)
-        _init_list_head.next = 8;
-    memcpy(sh_mem, &_init_list_head, 8);
-}
-
-static int device_open(struct inode *inode, struct file *file)
-{
-    if (!mutex_trylock(&mchar_mutex)) {
-        return -1;
-    } else {
-        share_mem_flag = 1;
-        write_index_lock();
-        pre_slot_len = 0;
-        write_index_unlock();
-        write_index = 8;
-        memset(sh_mem, '\0', MAX_SIZE);
-        do_init_share_mem(0);
-    }
-    return 0;
-}
-
-static int device_close(struct inode *indoe, struct file *file)
-{
-    mutex_unlock(&mchar_mutex);
-    share_mem_flag = -1;
-    return 0;
-}
-
-static int device_mmap(struct file *filp, struct vm_area_struct *vma)
-{
-    int ret = 0;
-    struct page *page = NULL;
-    unsigned long size = (unsigned long)(vma->vm_end - vma->vm_start);
-
-    vma->vm_flags |= 0;
-
-    if (size > MAX_SIZE) {
-        ret = -EINVAL;
-        goto out;
-    }
-
-    page = virt_to_page((unsigned long)sh_mem + (vma->vm_pgoff << PAGE_SHIFT));
-    ret = remap_pfn_range(vma, vma->vm_start, page_to_pfn(page), size, vma->vm_page_prot);
-    if (ret != 0) {
-        goto out;
-    }
-
-out:
-    return ret;
-}
-
-static const struct file_operations mchar_fops = {
-    .owner = THIS_MODULE,
-    .open = device_open,
-    .release = device_close,
-    .mmap = device_mmap,
-};
-
-static int get_read_index(void)
-{
-    int i;
-    struct sh_mem_list_head *_list_head;
-
-    for (i = 0; i < 8; i++)
-        list_head_char[i] = sh_mem[i];
-
-    _list_head = (struct sh_mem_list_head *)list_head_char;
-    return _list_head->read_index;
-}
-
-static struct msg_slot get_solt(int len, int next)
-{
-    struct msg_slot new_msg_slot = {len, next};
-    return new_msg_slot;
-}
-
-static int get_write_index(void)
-{
-    return write_index;
-}
-
-static void fix_write_index(int index)
-{
-    write_index = index;
-}
-
-static int send_msg_to_user_memshare(char *msg, int kfree_flag)
-{
-    int raw_data_len = 0;
-    int write_index_lock_flag = 0;
-    int curr_write_index = -1;
-    int now_write_index = -1;
-    int now_read_index = -1;
-    struct msg_slot new_msg_slot;
-
-    if (share_mem_flag == 1) {
-
-#if (WRITE_INDEX_TRY_LOCK == 1)
-        int i;
-        for (i = 0; i < WRITE_INDEX_TRY_LOCK_NUM; i++) {
-            if (write_index_trylock()) {
-                write_index_lock_flag = 1;
-                break;
-            }
-        }
-#else
-        write_index_lock();
-        write_index_lock_flag = 1;
-#endif
-
-        if (write_index_lock_flag) {
-
-            raw_data_len = strlen(msg);
-
-            if (raw_data_len == 0) {
-                write_index_unlock();
-                if (msg && kfree_flag == 1)
-                    kfree(msg);
-
-                return 0;
-            }
-
-            curr_write_index = get_write_index();
-
-            if (pre_slot_len != 0)
-                now_write_index = curr_write_index + 1;
-            else
-                now_write_index = curr_write_index;
-
-            if (check_read_index_flag == 1) {
-                now_read_index = get_read_index();
-                if (now_read_index > curr_write_index + 1) {
-                    if ((curr_write_index + 1024 + raw_data_len) > now_read_index)
-                        goto out;
-                }
-            }
-
-            if ((curr_write_index + CHECK_WRITE_INDEX_THRESHOLD) >= MAX_SIZE) {
-                now_read_index = get_read_index();
-                if (now_read_index <= CHECK_READ_INDEX_THRESHOLD) {
-#if (KERNEL_PRINT == 1)
-                    printk("READ IS TOO SLOW!! READ_INDEX:%d\n", now_read_index);
-#endif
-                    check_read_index_flag = 1;
-                }
-                else
-                    check_read_index_flag = -1;
-
-                new_msg_slot = get_solt(raw_data_len, 1);
-                memcpy(&sh_mem[now_write_index], &new_msg_slot, 8);
-                memcpy(&sh_mem[now_write_index + 8], msg, raw_data_len);
-                fix_write_index(7);
-
-#if (KERNEL_PRINT == 1)
-                printk("curr_write_index:%d pre_slot_len:%d now_write_index:%d now_read_index:%d\n",
-                       curr_write_index, pre_slot_len, now_write_index, now_read_index);
-#endif
-            } else {
-                new_msg_slot = get_solt(raw_data_len, -1);
-                memcpy(&sh_mem[now_write_index], &new_msg_slot, 8);
-                memcpy(&sh_mem[now_write_index + 8], msg, raw_data_len);
-                fix_write_index(now_write_index + 8 + raw_data_len);
-            }
-
-            pre_slot_len = raw_data_len + 8;
-            write_index_unlock();
-        }
-    }
-
-    if (msg && kfree_flag == 1)
-        kfree(msg);
-
-    return 0;
-
-out:
-    write_index_unlock();
-    if (msg && kfree_flag == 1)
-        kfree(msg);
-    return 0;
-}
-
-static int send_msg_to_user(char *msg, int kfree_flag)
-{
-#if (KERNEL_PRINT == 2)
-    printk("%s\n", msg);
-#endif
-
-#if (DELAY_TEST == 1)
-    if (kfree_flag == 1) {
-        send_msg_to_user_memshare(get_timespec(), 1);
-
-        if (msg)
-            kfree(msg);
-        return 0;
-    }
-#endif
-
-    return send_msg_to_user_memshare(msg, kfree_flag);
 }
 
 static int connect_ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
@@ -665,59 +398,6 @@ static int load_module_register_kretprobe(void)
 static void unregister_kretprobe_load_module(void)
 {
 	unregister_kretprobe(&load_module_kretprobe);
-}
-
-static int init_share_mem(void)
-{
-    int i;
-
-    major = register_chrdev(0, DEVICE_NAME, &mchar_fops);
-
-    if (major < 0) {
-        pr_err("[SMITH] REGISTER_CHRDEV_ERROR\n");
-        return -1;
-    }
-
-    class = class_create(THIS_MODULE, CLASS_NAME);
-    if (IS_ERR(class)) {
-        unregister_chrdev(major, DEVICE_NAME);
-        pr_err("[SMITH] CLASS_CREATE_ERROR");
-        return -1;
-    }
-
-    device = device_create(class, NULL, MKDEV(major, 0), NULL, DEVICE_NAME);
-    if (IS_ERR(device)) {
-        class_destroy(class);
-        unregister_chrdev(major, DEVICE_NAME);
-        pr_err("[SMITH] DEVICE_CREATE_ERROR");
-        return -1;
-    }
-
-    sh_mem = kzalloc(MAX_SIZE, GFP_KERNEL);
-
-    if (sh_mem == NULL) {
-        device_destroy(class, MKDEV(major, 0));
-        class_destroy(class);
-        unregister_chrdev(major, DEVICE_NAME);
-        pr_err("[SMITH] SHMEM_INIT_ERROR\n");
-        return -ENOMEM;
-    }
-    else {
-        for (i = 0; i < MAX_SIZE; i += PAGE_SIZE)
-            SetPageReserved(virt_to_page(((unsigned long)sh_mem) + i));
-    }
-
-    mutex_init(&mchar_mutex);
-    lock_init();
-    return 0;
-}
-
-static void uninstall_share_mem(void)
-{
-    device_destroy(class, MKDEV(major, 0));
-    class_unregister(class);
-    class_destroy(class);
-    unregister_chrdev(major, DEVICE_NAME);
 }
 
 static void uninstall_kretprobe(void)
