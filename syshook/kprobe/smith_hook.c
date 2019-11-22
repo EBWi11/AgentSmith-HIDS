634b64675e8a044645b20ce2a2ca8093ae51557a
E_Bwill <cy_sniper@yeah.net>
2019-11-22 11:24:53 +0800

    fix soime (kprobe version smith)

--- syshook/kprobe/smith_hook.c
+++ syshook/kprobe/smith_hook.c
@@ -156,7 +156,11 @@ static char *getfullpath(struct inode *inod,char *buffer,int len)
 		return NULL;
 
 	hlist_for_each(plist, &pinode->i_dentry) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
+		tmp = hlist_entry(plist, struct dentry, d_u.d_alias);
+#else
 		tmp = hlist_entry(plist, struct dentry, d_alias);
+#endif
 		if(tmp->d_inode == pinode) {
 			dent = tmp;
 			break;
