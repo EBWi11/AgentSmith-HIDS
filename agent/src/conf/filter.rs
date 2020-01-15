457b1430b1855c5b301b513a07f8779a42c54b23
E_Bwill <cy_sniper@yeah.net>
2020-01-15 14:32:06 +0800

    add some path for CREATE_FILE_ALERT_PATH

--- agent/src/conf/filter.rs
+++ agent/src/conf/filter.rs
@@ -1,5 +1,5 @@
 pub const EXE_WHITELIST: &'static [&'static str] = &["/usr/libexec/pk-command-not-found","/usr/bin/chronyc","/usr/lib64/sa/sa1","/usr/bin/date","/usr/lib64/sa","/usr/bin/test","/usr/sbin/chronyd","/usr/sbin/rsyslogd","/usr/bin/ls","/usr/bin/clear"];
 pub const CONNECT_DIP_WHITELIST: &'static [&'static str] = &["10.0.0.0/8", "172.16.0.0/16", "192.168.1.0/24", "127.0.0.1/24"];
 
-pub const CREATE_FILE_ALERT_PATH: &'static [&'static str] = &["/etc/","/usr/etc/","/usr/local/etc/","/usr/bin/","/usr/sbin/","/usr/local/bin/","/usr/local/sbin/","/usr/lib/","/usr/lib64/","/usr/local/lib/","/usr/local/lib64/","/sys/","/boot/","/var/lib/"];
+pub const CREATE_FILE_ALERT_PATH: &'static [&'static str] = &["/etc/","/usr/etc/","/usr/local/etc/","/usr/bin/","/usr/sbin/","/usr/local/bin/","/usr/local/sbin/","/usr/lib/","/usr/lib64/","/usr/local/lib/","/usr/local/lib64/","/sys/","/boot/","/var/lib/","/app/bin/","/app/sbin/","/root/bin/"];
 pub const CREATE_FILE_ALERT_CONTAINS: &'static [&'static str] = &[".sh",".php",".jsp",".asp",".aspx",".bash",".zsh",".csh",".service",".ini",".conf",".ko",".so",".d",".a",".htaccess","backdoor"];
