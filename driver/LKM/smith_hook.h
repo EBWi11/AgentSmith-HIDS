c5b3f1f1e0a23498eea17ec16566b66939ab6e4e
E_Bwill <cy_sniper@yeah.net>
2020-03-05 10:41:24 +0800

    update

--- driver/LKM/smith_hook.h
+++ driver/LKM/smith_hook.h
@@ -46,6 +46,18 @@
 typedef unsigned short int uint16;
 typedef unsigned long int uint32;
 
+
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,32)
+#define __ARG_PLACEHOLDER_1 0,
+#define config_enabled(cfg) _config_enabled(cfg)
+#define _config_enabled(value) __config_enabled(__ARG_PLACEHOLDER_##value)
+#define __config_enabled(arg1_or_junk) ___config_enabled(arg1_or_junk 1, 0)
+#define ___config_enabled(__ignored, val, ...) val
+
+#define IS_ENABLED(option) \
+        (config_enabled(option) || config_enabled(option##_MODULE))
+#endif
+
 #define NIPQUAD(addr) \
     ((unsigned char *)&addr)[0], \
     ((unsigned char *)&addr)[1], \
